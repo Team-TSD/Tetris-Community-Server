95d31896a17b27a245358a7cc0ff47d57171ed45
Matt <mzhu103@gmail.com>
2023-06-20 10:12:11 -0500

    upstream bug on vps?

--- src/commit.sh
+++ src/commit.sh
@@ -8,7 +8,7 @@ git checkout -b $branch_name
 cat > tetriscommunity.md
 git add tetriscommunity.md
 git commit -m "$1" -m "$2"
-git push
+git push --set-upstream origin $branch_name
 git checkout main
 git branch --delete $branch_name
 } 2>&1
